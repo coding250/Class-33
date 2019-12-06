5c0fcaca74de34a26e2f38632f20a858f954bcb2
Arohi <arohi@Arohis-MacBook-Air.local>
2019-12-06 17:45:39 +0530

    Debugging

--- sketch.js
+++ sketch.js
@@ -56,6 +56,7 @@ function draw(){
         text("Score  " + score, width-300, 50)
     
     Engine.update(engine);
+    console.log(bird.body.speed);
     //strokeWeight(4);
     box1.display();
     box2.display();
@@ -93,7 +94,9 @@ function mouseReleased(){
 }
 
 function keyPressed(){
-    if(keyCode === 32){
+    if(keyCode === 32 && bird.body.speed<1){
+        bird.trajectory = [];
+        Matter.Body.setPosition(bird.body,{x:200,y:50});
        slingshot.attach(bird.body);
     }
 }
